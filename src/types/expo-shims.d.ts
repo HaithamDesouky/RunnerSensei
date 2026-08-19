5e91e73616e941934b6d2221b0af34c455a45525
Haitham Desouky <haithamadesouky@gmail.com>
2026-08-19 13:39:56 +0200

    fix import error

---
+++ src/types/expo-shims.d.ts
@@ -0,0 +1,7 @@
+declare module "expo-constants";
+declare module "expo-file-system";
+declare module "expo-file-system/legacy";
+declare module "expo-image-picker";
+declare module "expo-secure-store";
+declare module "expo";
+
