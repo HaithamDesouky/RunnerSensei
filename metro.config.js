1dab422e34aaa1a1319cf09a5dbfbdf0b54e2cfe
Haitham Desouky <haithamadesouky@gmail.com>
2026-08-20 16:22:30 +0200

    fix

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,7 @@ try {
   ({ getDefaultConfig } = require("expo/metro-config"));
 }
 
-const path = require('path');
+const path = require("path");
 const config = getDefaultConfig(__dirname);
 
 // Redirect all react-native-maps imports to a no-op stub on web
@@ -22,12 +22,12 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
 config.resolver = config.resolver || {};
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
-  'prop-types': path.resolve(__dirname, 'src/shims/prop-types', 'index.js'),
+  "prop-types": path.resolve(__dirname, "src/shims/prop-types", "index.js"),
 };
 
 config.watchFolders = [
   ...(config.watchFolders || []),
-  path.resolve(__dirname, 'src/shims'),
+  path.resolve(__dirname, "src/shims"),
 ];
 
 module.exports = config;
