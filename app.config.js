57ff086db9d226191e3a1d296c8c8ce424f4de7f
Haitham Desouky <haithamadesouky@gmail.com>
2026-08-20 16:19:10 +0200

    build apk

--- app.config.js
+++ app.config.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = ({ config }) => ({
   ...config,
   extra: {
+    ...(config.extra || {}),
     SUPABASE_URL: process.env.SUPABASE_URL || "",
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || "",
   },
 });
+
