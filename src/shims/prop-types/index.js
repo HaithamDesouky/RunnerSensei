d831ad2424f15f53a5dbeffac9068a4b9a86d686
Haitham Desouky <haithamadesouky@gmail.com>
2026-08-20 16:22:23 +0200

    fix

---
+++ src/shims/prop-types/index.js
@@ -0,0 +1,29 @@
+function createTypeChecker() {
+  const fn = function () { return null; };
+  fn.isRequired = fn;
+  return fn;
+}
+
+const PropTypes = {
+  any: createTypeChecker(),
+  array: createTypeChecker(),
+  bool: createTypeChecker(),
+  func: createTypeChecker(),
+  number: createTypeChecker(),
+  object: createTypeChecker(),
+  string: createTypeChecker(),
+  symbol: createTypeChecker(),
+  node: createTypeChecker(),
+  element: createTypeChecker(),
+  instanceOf: () => createTypeChecker(),
+  oneOf: () => createTypeChecker(),
+  oneOfType: () => createTypeChecker(),
+  arrayOf: () => createTypeChecker(),
+  objectOf: () => createTypeChecker(),
+  shape: () => createTypeChecker(),
+  exact: () => createTypeChecker(),
+  checkPropTypes: () => {},
+};
+
+module.exports = PropTypes;
+exports.default = PropTypes;
