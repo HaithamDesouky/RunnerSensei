b8e3bad814c2b06592756d7c3cb9f8a9fc02a60e
Haitham Desouky <haithamadesouky@gmail.com>
2026-08-20 16:22:23 +0200

    fix

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,7 @@ try {
   ({ getDefaultConfig } = require("expo/metro-config"));
 }
 
+const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 // Redirect all react-native-maps imports to a no-op stub on web
@@ -16,6 +17,18 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (originalResolver) return originalResolver(context, moduleName, platform);
   return context.resolveRequest(context, moduleName, platform);
 };
+
+// Map 'prop-types' to local shim when real package is not installed
+config.resolver = config.resolver || {};
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  'prop-types': path.resolve(__dirname, 'src/shims/prop-types', 'index.js'),
+};
+
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(__dirname, 'src/shims'),
+];
 
 module.exports = config;
 
